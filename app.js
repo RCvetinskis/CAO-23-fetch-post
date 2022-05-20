09a7910fda45a50b307974b80de8bc62b256cdf1
RCvetinskis <robertas.cvetinskis@gmail.com>
2022-05-20 09:47:16 +0300

    sutrumpintas kodas

--- app.js
+++ app.js
@@ -4,8 +4,8 @@
 // Sukurkite papildomą puslapį, add.html, kuriame būtų forma su dviem inputais - brand ir model; šie paduotų su post'u informaciją į base url (formatas: objektas su dviem properties: brand ir model).
 // 3.Sukurkite notification - t.y. sėkmingai užpildžius formą ir gavus patvirtinimą, turi virš formos rašyti, kad duomenys sėkmingai išsaugoti; o jei blogai - išmesti errorą.
 const form = document.querySelector("form")
-let inputBrand = document.getElementById("brand")
-let  inputModel = document.getElementById("model")
+const inputBrand = document.getElementById("brand")
+const  inputModel = document.getElementById("model")
 const text = document.getElementById("text")
 const URL = "https://olive-bead-glazer.glitch.me"
 
@@ -13,21 +13,20 @@ const URL = "https://olive-bead-glazer.glitch.me"
 
 
    form.addEventListener("submit", function(e){
+    e.preventDefault()
     let car ={
-        brand: "audi",
-        model: "a4"
+        brand: inputBrand.value,
+        model: inputModel.value
      
     }
-    e.preventDefault()
-    if(car["brand"] = inputBrand.value ){
-        car["brand"] = inputBrand.value
-        if (car["model"] = inputModel.value){
-            car["model"] = inputModel.value
-        }
-        console.log(car)
-    }
-     form.value = ""
+    sendData(car)
 
+
+   })
+
+
+
+   function sendData(car){
     fetch(URL, {
         method: 'POST',
         headers: {
@@ -40,9 +39,5 @@ const URL = "https://olive-bead-glazer.glitch.me"
        .then(results =>
         text.innerText = results.msg)
        .catch(e=> text.innerText = e)
-      
 
-  
-   })
-
-
+   } 
